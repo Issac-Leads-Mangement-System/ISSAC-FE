5aacde182cd1e245be5236d45ddb3daa07a58433
Poroch Marcel <marcel.poroch@assist.ro>
2024-12-06 14:03:04 +0200

    add cards

--- src/store/Jobs/job-stats-store.ts
+++ src/store/Jobs/job-stats-store.ts
@@ -3,73 +3,41 @@ import api from "../../api";
 import useNotificationStore from "../Notification/notification-store";
 
 interface IJobsState {
-  job: {
-    job_name: string;
-    type_id: number | undefined;
-    free_leads: number | undefined;
-  };
-  userTeam: any[];
-  jobs: any[];
+ activeJob: string
+ jobById: any
+ jobLeadsById: any
 }
 
-const jobsStore = create<IJobsState>((set) => ({
-  job: {
-    job_name: "",
-    type_id: undefined,
-    free_leads: undefined,
-  },
-  userTeam: [],
-  jobs: [],
-  inputValues: [],
+const jobStatsStore = create<IJobsState>((set) => ({
+  activeJob: "",
+  jobById: {},
+  jobLeadsById: [],
 
-  setJob: (value: any, key: string) =>
-    set((state) => ({
-      job: {
-        ...state.job,
-        [key]: value,
-      },
-    })),
-
-  getFreeLeads: async () => {
-    const { job } = jobsStore.getState();
-    const response = await api.get(
-      `${process.env.REACT_APP_BASE_URL}/jobs/free_leads/${job.type_id}`
-    );
-    set((state) => ({
-      job: {
-        ...state.job,
-        free_leads: response.data.total_amount_of_free_leads,
-      },
-    }));
+  setKey: (key: string, value: string) =>  {
+    set({[key]: value});
   },
 
-  getUserTeam: async () => {
+  getJobById: async () => {
     try {
-      const response = await api.post(
-        `${process.env.REACT_APP_BASE_URL}/users/?page=${1}&limit=${50}`,
-        {
-          user_role: ["employee"],
-          user_status: ["active"],
-          team_id: [1],
-        }
-      );
-
-      set({ userTeam: response.data.users_response });
+      const { activeJob } = jobStatsStore.getState();
+      const response = await api.get(`
+        ${process.env.REACT_APP_BASE_URL}/jobs/${activeJob}
+        `)
+        set({jobById: response.data})
     } catch (err) {
-      console.log("zzz err", err);
+      console.log('err', err)
     }
   },
 
-  getAllJobs: async () => {
+  getJobLeadsById: async () => {
     try {
-      const response = await api.post(
-        `${process.env.REACT_APP_BASE_URL}/jobs/?page=${1}&limit=${50}`
-      );
-      set({ jobs: response.data.jobs_response });
+      const { activeJob } = jobStatsStore.getState();
+      const response = await api.post(`${process.env.REACT_APP_BASE_URL}/jobs/${activeJob}/leads`);
+      set({jobLeadsById: response.data.job_leads_response})
     } catch (err) {
-      console.log("err", err);
+      console.log('err', err)
     }
-  },
+  }
 }));
 
-export default jobsStore;
+export default jobStatsStore;
